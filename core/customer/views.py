a5a9e9654595721183a42cb3ea4a1fafb6f676cb
kesav99 <bkesav183@gmail.com>
2021-10-22 15:10:59 +0530

    commit Authentication

--- core/customer/views.py
+++ core/customer/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import  render,redirect
 from django.contrib.auth.decorators import login_required
 from core.customer import forms
 from django.urls import reverse
+
+from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+
 @login_required()
 def home(request):
     return redirect(reverse('customer:profile'))
@@ -9,15 +14,27 @@ def home(request):
 def profile_page(request):
     user_form=forms.BasicUserForm(instance=request.user)
     customer_form=forms.BasicCustomerForm(request.POST,instance=request.user.customer)
+    password_form=PasswordChangeForm(request.user)
 
     if request.method=="POST":
+
+     if request.POST.get('action') == "upadte_profile":
         user_form=forms.BasicUserForm(request.POST,instance=request.user)
         customer_form=forms.BasicCustomerForm(request.POST,request.FILES,instance=request.user.customer) 
         if user_form.is_valid() and customer_form.is_valid:
             user_form.save()
             customer_form.save()
+            messages.success(request,'Your Profile has been updated')
             return redirect(reverse('customer:profile'))
-    return render(request,'customer/profile.html', {
+        elif  request.POST.get('action') == "upadte_password":
+         password_form=PasswordChangeForm(request.user,request.POST)
+         if password_form.is_valid():
+            user=password_form.save()
+            update_session_auth_hash(request,user)
+            messages.success(request,'Your Password has been updated')
+            return redirect(reverse('customer:profile'))
+        return render(request,'customer/profile.html', {
         "user_form": user_form,
-        "customer_form": customer_form
+        "customer_form": customer_form,
+        "password_form": password_form
     })
